c8da2bc6199275eb2c093d9ce06cb8afa7a372ed
ahmedfci93 <ahmed.fci93@gmail.com>
2018-01-14 23:55:18 +0200

    First Commit

---
+++ GeeksForGeeks/Arrays/Fighting the darkness.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static PrintWriter out;
+    
+    public static void main(String[] args) {
+        FastReader sc = new FastReader();
+        out = new PrintWriter(new BufferedOutputStream(System.out), true);
+        int t,n,m;
+        t = sc.nextInt();
+        while (t > 0) {
+            m=sc.nextInt();
+            n=0;
+            for (int i = 0; i < m; i++) {
+                n=Math.max(n, sc.nextInt());
+            }
+            System.out.println(n);
+            t--;
+        }
+        out.close();
+    }
+
+    static class FastReader {
+
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
