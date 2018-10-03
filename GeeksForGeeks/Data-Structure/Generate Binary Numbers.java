137676420bd0103e59f53445fa8e47ee0d8f7d8a
ahmedfci93 <ahmed.fci93@gmail.com>
2018-10-03 22:12:57 +0200

    Committed

---
+++ GeeksForGeeks/Data-Structure/Generate Binary Numbers.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+
+public class GFG {
+
+
+    public static PrintWriter out;
+
+    public static void main(String[] args) {
+        FastReader sc = new FastReader();
+        out = new PrintWriter(new BufferedOutputStream(System.out), true);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            for (int i = 1; i <= n; i++) {
+                StringBuilder ans = new StringBuilder();
+                int x = i;
+                while (x != 0) {
+                    ans.append(x & 1);
+                    x >>= 1;
+                }
+                out.print(ans.reverse()+" ");
+            }
+            out.println();
+        }
+        out.close();
+    }
+
+    static class FastReader {
+
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
